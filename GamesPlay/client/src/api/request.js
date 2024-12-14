9a1c4fa933ec0acd9a668dcb0301358891d15a24
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-14 14:23:36 +0200

    create requester

---
+++ GamesPlay/client/src/api/request.js
@@ -0,0 +1,34 @@
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {},
+    };
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.status == 204) {
+            return response;
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const requester = {
+    get: (url) => request('GET', url),
+    post: (url, data) => request('POST', url, data),
+    put: (url, data) => request('PUT', url, data),
+    del: (url) => request('DELETE', url),
+};
+
+export default requester;
