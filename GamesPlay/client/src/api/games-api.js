1e716b33e6fb95cb18096f007a6dbf50630145be
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-14 14:23:52 +0200

    create gameAPI

---
+++ GamesPlay/client/src/api/games-api.js
@@ -0,0 +1,43 @@
+import requester from './request.js';
+
+const host = 'http://localhost:3030';
+
+const endpoints = {
+    all: '/data/games?sortBy=_createdOn%20desc',
+    new: '/data/games?sortBy=_createdOn%20desc&distinct=category',
+    create: '/data/games',
+    one: (id) => `/data/games/${id}`,
+};
+
+async function getAll() {
+    return requester.get(host + endpoints.all);
+}
+
+async function getNew() {
+    return requester.get(host + endpoints.new);
+}
+
+async function getOne(gameId) {
+    return requester.get(host + endpoints.one(gameId));
+}
+
+async function create(data) {
+    return requester.post(host + endpoints.create, data);
+}
+
+async function updateById(gameId, data) {
+    return requester.put(host + endpoints.one(gameId), data);
+}
+
+async function deleteById(gameId) {
+    return requester.del(host + endpoints.one(gameId));
+}
+
+const gamesAPI = {
+    getAll,
+    getNew,
+    getOne,
+    create,
+    updateById,
+    deleteById,
+};
